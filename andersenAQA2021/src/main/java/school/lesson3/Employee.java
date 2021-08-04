cbd29c0d52fb2610e84345604eb4725b14c6894e
MariaSerdukova <MariaSerdukova>
2021-08-04 23:14:53 +0300

    Lesson3 commit

---
+++ andersenAQA2021/src/main/java/school/lesson3/Employee.java
@@ -0,0 +1,41 @@
+package school.lesson3;
+
+public class Employee {
+
+    private String name;
+    private String post;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String name, String post, String email, String phone, double salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+
+    public String toString() {
+        return name + ", " + post + ", " + email + ", " + phone + ", " + salary + ", " + age;
+    }
+
+    public static void main(String[] args) {
+        Employee[] empArray = new Employee[5];
+
+        empArray[0] = new Employee("Сердюкова Мария Анатольевна", "Ассистент", "aaa@gmail.com", "786 26 78", 1190.00,26);
+        empArray[1] = new Employee("Иванов Иван Иванович", "Профессор","bbb@gmail.com", "796 16 52", 3000.00, 50);
+        empArray[2] = new Employee("Петров Петр Петрович", "Доцент", "ccc@gmail.com", "536 00 23", 2500.50, 35);
+        empArray[3] = new Employee("Сидоров Сидор Сидорович", "Старший преподаватель", "ddd@gmail.com", "112 06 83", 2000.00, 33);
+        empArray[4] = new Employee("Шариков Александр Сергеевич", "Доцент", "eee@gmail.com", "123 45 78", 2500.50, 45);
+
+        for (Employee emp : empArray) {
+            if (emp.age > 40) {
+                System.out.println(emp.toString());
+            }
+        }
+    }
+}
